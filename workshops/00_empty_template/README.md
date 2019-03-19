b04610b112de3c9922580971477953fefc601e0b
Max Frischknecht <max@debutdebut.com>
2019-03-19 18:13:07 +0100

    Created boilerplate sketch

---
+++ workshops/00_empty_template/README.md
@@ -0,0 +1,2 @@
+# P5.js Starter Template
+This is a "empty" starter template for you to go creative 🌈
---
+++ workshops/00_empty_template/sketch.js
@@ -0,0 +1,9 @@
+// this executes once at the beginning
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+// this executes 60 times a second
+function draw() {
+
+}
